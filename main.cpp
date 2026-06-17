da10f326b7cec4a1e519db828aa37f709a6c5d7b
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2026-06-17 21:52:12 +0530

    Learned how to change the colors in the rendered screen

--- main.cpp
+++ main.cpp
@@ -51,6 +51,10 @@ int main()
 		// -----
 		processInput(window);
 
+		// Changing the render color to green!
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
